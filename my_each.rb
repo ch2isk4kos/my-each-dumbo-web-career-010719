a4dbec966668cbe8d1d8396e6488dcbc0504a2f8
Chris Kakos <ckakos@gmail.com>
2018-12-10 19:34:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+    # code here
+    array = collection.size
+    n = 0
+    while n > array
+        collection[n]
+        n+=1
+    end
+    return collection
 end
